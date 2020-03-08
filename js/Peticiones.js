550718d50c8d2f388383bf37a1c2bf4e500dc5da
Cristhian Rodriguez <crisdrodriguezm@hotmail.com>
2020-03-08 16:49:15 -0500

    agregando metodo para obtener los objetos del api

---
+++ js/Peticiones.js
@@ -0,0 +1,15 @@
+console.log(doGet('https://www.datos.gov.co/resource/x84b-t6fw.json'))
+
+// funtions
+function doGet(url) {
+    var json;
+    fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (myJson) {
+            console.log(myJson)
+            json = myJson
+            
+        });
+}
